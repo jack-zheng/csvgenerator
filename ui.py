b444d2d92156c2ec05d0c4e9cf59483af9524ee0
I306454 <jiabin.zheng01@sap.com>
2018-10-28 15:57:52 +0800

    finish style update

--- ui.py
+++ ui.py
@@ -1,46 +1,26 @@
 from tkinter import *
-from tkinter.ttk import Frame, Button, Style
 from tkinter import filedialog
-import os
 from generator import *
-
-
-class Checkbar(Frame):
-    def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
-        Frame.__init__(self, parent)
-        self.status_map = {}
-        ret = [picks[i:i+6] for i in range(0, len(picks), 5)]
-        for row, val in enumerate(ret):
-            for col, pick in enumerate(val):
-                var = IntVar()
-                if pick in ['USERNAME', 'USERID']:
-                    var.set(1)
-                chk = Checkbutton(self, text=pick, variable=var)
-                chk.grid(row=row, column=col)
-                self.status_map[chk.cget('text')] = var
+import os
 
 
 def main():
     root = Tk()
-    root.geometry('500x700')
     root.title("User File Generator")
 
     file_path = StringVar()
     user_count = IntVar()
     status_map = {}
 
-    style = Style()
-    style.theme_use("default")
-
     def append_checkbox(picks):
-        ret = [picks[i:i+6] for i in range(0, len(picks), 5)]
+        ret = [picks[i:i+3] for i in range(0, len(picks), 3)]
         for row, val in enumerate(ret):
             for col, pick in enumerate(val):
                 var = IntVar()
                 if pick in ['USERNAME', 'USERID']:
                     var.set(1)
                 chk = Checkbutton(checkbox_frame, text=pick, variable=var)
-                chk.grid(row=row, column=col)
+                chk.grid(row=row, column=col, sticky=W, padx=5)
                 status_map[chk.cget('text')] = var
 
     def onClick():
@@ -53,32 +33,32 @@ def main():
         file_path.set(abspath)
 
     file_select_frame = Frame(root, relief=RAISED, borderwidth=1)
-    file_select_frame.pack(fill=BOTH, expand=True)
+    file_select_frame.pack(fill=BOTH)
 
     lbl = Label(file_select_frame, text="File Path")
     lbl.pack(side=LEFT)
     txt = Entry(file_select_frame, textvariable=file_path)
-    txt.pack(side=LEFT)
+    txt.pack(side=LEFT, expand=1, fill='both')
     btn = Button(file_select_frame, text='Browse', command=onClick)
-    btn.pack(side=LEFT)
+    btn.pack(side=RIGHT)
 
     header_frame = Frame(root, relief=RAISED, borderwidth=1)
-    header_frame.pack(fill=BOTH, expand=True)
+    header_frame.pack(fill=BOTH)
     desclbl = Label(header_frame, text='All Header Supported')
     desclbl.pack(side=TOP)
-    checkbox_frame = Frame(header_frame, relief=RAISED, borderwidth=1)
-    checkbox_frame.pack(fill=BOTH, expand=True)
+    checkbox_frame = Frame(header_frame, width=300, height=400, relief=RAISED, borderwidth=1)
+    checkbox_frame.pack(fill=BOTH)
 
 
     user_count_frame = Frame(root, relief=RAISED, borderwidth=1)
-    user_count_frame.pack(fill=BOTH, expand=True)
+    user_count_frame.pack(fill=BOTH)
     spinlbl = Label(user_count_frame, text="How manay user you want?")
     spinlbl.pack(side=LEFT)
     spin = Spinbox(user_count_frame, from_=0, to=100, width=5, textvariable=user_count)
     spin.pack(side=LEFT)
 
     submit_frame = Frame(root, relief=RAISED, borderwidth=1)
-    submit_frame.pack(fill=BOTH, expand=True)
+    submit_frame.pack(fill=BOTH)
 
     def process_submit():
         print('generate file start !')
